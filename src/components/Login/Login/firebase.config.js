4122e5ec4e0a2d10d7f7caf609f5b571d8ee2b1f
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-04-16 23:04:41 +0600

    first commit

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1JAtwsiSjoKBnT1I86Oa6KukDlhahIk",
+  authDomain: "doctorsportal-9ccb2.firebaseapp.com",
+  projectId: "doctorsportal-9ccb2",
+  storageBucket: "doctorsportal-9ccb2.appspot.com",
+  messagingSenderId: "79133480252",
+  appId: "1:79133480252:web:baf5d2b035bcf45412ba39"
+};
+
+export default firebaseConfig;
